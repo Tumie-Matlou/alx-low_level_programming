40a188d6f48ad5d3289dbd223b98c3785fdcdbcc
Tumelo Matlou <anonymoomen@gmail.com>
2023-02-16 15:35:58 -0800

    a script that generates the assembly code (Intel syntax) of a C code and save it in an output file.

---
+++ 0x00-hello_world/7-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * a program that assembles the intel synta
+ * a program that assembles the intel syntax
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
+}
+
