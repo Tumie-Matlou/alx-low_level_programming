0100a4c9e768e79bd6ce64ab73c607bf2ce56c83
Tumie-Matlou <123845948+Tumie-Matlou@users.noreply.github.com>
2023-02-19 23:38:16 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,16 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
+{	char c; /* initialising the 'char' to c */
+	int i; /* initialising the 'int' to i */
+	long li; /* intialising the 'long int' to li */
+	long long lli; /* intialising the 'long long int' to lli */
+	float f; /* intialising the 'float' to f */
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c)); /* print the size of 'char' in bytes on the screen */
+	printf("Size of an int: %ld byte(s)\n", sizeof(i)); /* print the size of 'int' in bytes on the screen */
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li)); /* print the size of 'long int' in bytes on the screen */
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli)); /* print the size of 'long long int' in bytes on the screen */
+	printf("Size of a float: %ld byte(s)\n", sizeof(f)); /* print the size of 'float' in bytes on the screen */
 		return (0);
 }
