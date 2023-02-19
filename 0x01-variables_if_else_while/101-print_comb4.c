cc1fc3e19b6c71db8ad1168645e5c925dc692858
Tumie-Matlou <123845948+Tumie-Matlou@users.noreply.github.com>
2023-02-20 00:08:29 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,16 +11,16 @@ int main(void)
 	int ten;
 	int hundred;
 
-	for (hundred = '0'; hundred <= '9'; hundred++) /*hundreds place*/
+	for (hundred = '0'; hundred <= '9'; hundred++) /* setting hundreds iteration, with incrementation */
 	{
-		for (ten = (hundred + 1); ten <= '9'; ten++) /*tens=100s+1*/
+		for (ten = (hundred + 1); ten <= '9'; ten++) /* tens = hundreds + 1, with incrementation*/
 		{
-			for (one = (ten + 1); one <= '9'; one++) /*ones*/
+			for (one = (ten + 1); one <= '9'; one++) /*ones = tens + 1, with incrementation*/
 			{
 				putchar(hundred);
 				putchar(ten);
 				putchar(one);
-				if (hundred != '7' || ten != '8' || one != '9')
+				if (hundred != '7' || ten != '8' || one != '9') /* condition not to place comma and space after last possible combination */
 				{
 					putchar(',');
 					putchar(' ');
