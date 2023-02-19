dba75f0cc8dd3b6df3d9a68a04d47775eb7418ea
Tumie-Matlou <123845948+Tumie-Matlou@users.noreply.github.com>
2023-02-20 00:38:37 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,14 +10,14 @@ int main(void)
 	int n = '0';
 	int a_to_f = 'a';
 
-	while (n <= '9')
+	while (n <= '9') /* proceed as long as variable n is less or equal to 9 */
 	{	putchar(n);
-		n++;
+		n++; /* increment n */
 	}
 
-	while (a_to_f <= 'f')
+	while (a_to_f <= 'f') /* proceed a long as variable a_to_f is letters a and/between f */
 	{	putchar(a_to_f);
-		a_to_f++;
+		a_to_f++; /* increment by letter */
 	}
 	putchar('\n');
 	return (0);
