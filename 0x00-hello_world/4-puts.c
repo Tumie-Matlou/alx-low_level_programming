7a621e3017c97faf5465fbaee87795cda4274508
Tumie-Matlou <123845948+Tumie-Matlou@users.noreply.github.com>
2023-02-19 23:28:51 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{	puts("\"Programming is like building a multilingual puzzle"); /* 'puts' prints the text in  double quotes on the standard output */
 		return (0);
 }
