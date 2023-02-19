1e06fd6c9e9f9b3610c369960a60773db5b8a31b
Tumie-Matlou <123845948+Tumie-Matlou@users.noreply.github.com>
2023-02-20 00:21:19 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,9 @@
  * Return: 0 (success)
  */
 int main(void)
-{	char alph = 'a';
+{	char alph = 'a'; /* initialising a char named 'alph' to a*/
 
-	for (alph = 'a'; alph <= 'z'; alph++)
+	for (alph = 'a'; alph <= 'z'; alph++) /* alph is letter a throughout the alphabet to z, increment by a letter */
 	{	putchar(alph);
 	}
 	putchar('\n');
