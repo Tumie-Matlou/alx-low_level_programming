83bb7fecf5b5b54e61f6e74805d7ccf371005e9a
Tumie-Matlou <123845948+Tumie-Matlou@users.noreply.github.com>
2023-02-19 23:31:33 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{	printf("with proper grammar, but the outcome is a piece of art,\n");
+{	printf("with proper grammar, but the outcome is a piece of art,\n"); /* 'printf' displays the text in double quotes onto the standard output */
 		return (0);
 }
