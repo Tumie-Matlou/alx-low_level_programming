ebcb74182b338c87d218e6d77a8ef7c4635c991d
Tumie-Matlou <123845948+Tumie-Matlou@users.noreply.github.com>
2023-02-20 00:24:35 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,12 @@
  * Return: 0 (success)
  */
 int main(void)
-{	char al = 'a', AL = 'A';
+{	char al = 'a', AL = 'A'; /* initialising lowercase alphabets(al) to a and uppercase alphabets(AL) to A */
 
-	for (al = 'a'; al <= 'z'; al++)
+	for (al = 'a'; al <= 'z'; al++) /* iterate from leter a and terminate at z, increment by a letter */
 	{	putchar(al);
 	}
-	for (AL = 'A'; AL <= 'Z'; AL++)
+	for (AL = 'A'; AL <= 'Z'; AL++) /* iterate from leter A and terminate at Z, increment by a letter */
 	{	putchar(AL);
 	}
 	putchar('\n');
