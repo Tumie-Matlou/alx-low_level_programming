682c95511bddb7157a26c1095a1d3a5161f3647b
Tumelo Matlou <anonymoomen@gmail.com>
2023-03-01 07:45:02 -0800

    fixing bug

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,21 +10,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
+	int i = 0;/*declare integers*/
 	int j = 0;
 
 	while (*(dest + i))
 	{
-		i++;
+		i++;/*point to the last index of dest(i.e \0)*/
 	}
 
 	while (*(src + j))
 	{
-		*(dest + i) = *(src + j);
-		i++;
-		j++;
+		*(dest + i) = *(src + j);/*null dest index = start point to src string*/
+		i++;/*move to next index of dest*/
+		j++;/*move to next index of src*/
 	}
-	*(dest + i) = '\0';
+	*(dest + i) = '\0';/*address of dest is \0*/
 
 	return (dest);
 }
