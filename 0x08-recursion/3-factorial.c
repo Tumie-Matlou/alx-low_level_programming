5bfd07396984dfb49a21dc7fbee8c7502c283370
Tumelo Matlou <123845948+Tumie-Matlou@users.noreply.github.com>
2023-03-13 19:14:56 +0200

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,10 +10,10 @@
 
 int factorial(int n)
 {
-	if (n < 0)
+	if (n < 0) /*if number is less than 0, return -1*/
 		return (-1);
-	if (n == 1)
+	if (n == 1) /*base case, exit function and return 1*/
 		return (1);
 	else
-		return (n * factorial(n - 1));
+		return (n * factorial(n - 1)); /*recursive case, solve the problem*/
 }
