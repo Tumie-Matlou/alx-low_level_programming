6bbae7b20abbeddcbd6e4ea5f2924e8367249072
Tumelo Matlou <anonymoomen@gmail.com>
2023-03-20 13:12:05 -0700

    a program that prints the name of the file it was compiled from, followed by a new line.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a function that prints the name of the file using a macro
+ *
+ * Return: 0  on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
