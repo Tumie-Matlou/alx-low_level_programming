345ed8bf8ed50ae5303767f6de3fa492ea9adfae
Tumelo Matlou <anonymoomen@gmail.com>
2023-03-22 13:46:51 -0700

    fixing bug

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,13 @@
 
 /**
  * print_name - prints the name of a person
- * @name: the name of a person
+ * @name: the name of a person (function parameter)
  * @f: pointer to a function
 */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name == NULL && f == NULL)
+		return;
+	f(name);
 }
