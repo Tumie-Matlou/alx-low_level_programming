8fc9e3262046ed926b408295fd2e2dc200cc9f06
Tumelo Matlou <anonymoomen@gmail.com>
2023-04-04 15:46:52 -0700

    fixing bug: it was a missing null check for the head pointer. Smh + facepalm

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,6 +14,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *delete;
 	unsigned int i;
 
+	if (*head == NULL)
+		return (-1);
+
 	if (index == 0)
 	{
 		*head = temp->next;
