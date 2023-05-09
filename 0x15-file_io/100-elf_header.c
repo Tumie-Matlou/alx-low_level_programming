8e73011c7fdb5b5b59952846df0d926102a59659
Tumelo Matlou <Anonymoomen@gmail.com>
2023-05-08 18:30:49 -0700

    fixing some bugs

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,13 +5,12 @@
  * elf_identity - a function that prints the e_ident field of ELF header
  * Magic, Class, Data
  */
-void elf_identity(void)
+void elf_identity(Elf64_Ehdr header)
 {
-	uint32_t magic = *(uint32_t*)(&header.e_ident[0]);
 	uint8_t class = header.e_ident[EI_CLASS];
 	uint8_t endian = header.e_ident[EI_DATA];
 	int i;
-
+	
 	printf("Magic: ");
 	for (i = 0; i < EI_NIDENT; i++)
 	{
@@ -49,9 +48,9 @@ void elf_identity(void)
 /**
  * ver - prints the version of the ELF header
  */
-void ver(void)
+void ver(Elf64_Ehdr header)
 {
-	uint32_t version;
+	uint32_t version = 0;
 
 	switch (header.e_ident[EI_CLASS])
 	{
@@ -71,11 +70,9 @@ void ver(void)
 /**
  * osabi - prints the OS/ABI of the ELF header
  */
-void osabi(void)
+void osabi(Elf64_Ehdr header)
 {
-	uint8_t os_abi = header.e_ident[EI_OSABI];
-
-	switch (header->e_ident[EI_OSABI])
+	switch (header.e_ident[EI_OSABI])
 	{
 		case ELFOSABI_SYSV:
 			printf("OS/ABI: UNIX System V\n");
@@ -107,16 +104,15 @@ void osabi(void)
 		default:
 			printf("OS/ABI: Unknown\n");
 			break;
+	}
 }
 
 /**
  * abi - prints the ABI Version of the ELF
  */
-void abi(void)
+void abi(Elf64_Ehdr header)
 {
-	uint8_t abi_version = header.e_ident[EI_ABIVERSION];
-
-	switch (header->e_ident[EI_ABIVERSION])
+	switch (header.e_ident[EI_ABIVERSION])
 	{
 		case 0:
 			printf("ABI Version: 0\n");
@@ -136,12 +132,9 @@ void abi(void)
 /**
  * type_and entry - a function that prints the type and entry point fields
  */
-void type_and_entry(void)
+void type_and_entry(Elf64_Ehdr header)
 {
-	uint16_t file_type = header.e_type;
-	Elf64_Addr entry_point = header.e_entry;
-
-	switch (header->e_type)
+	switch (header.e_type)
 	{
 		case ET_NONE:
 			printf("Type: No file type\n");
@@ -162,7 +155,7 @@ void type_and_entry(void)
 			printf("Type: Unknown\n");
 			break;
 	}
-	printf("%lx\n", header->e_entry);
+	printf("Entry point address: %lX\n", header.e_entry);
 }
 
 /**
@@ -176,15 +169,17 @@ int main(int argc, char *argv[])
 	int fd;
 	Elf64_Ehdr header;
 	ssize_t bytes_read;
+	off_t offset;
 
-	if (argc != 1)
+	if (argc != 2)
 	{
-		perrr("Invalid number of arguments")
+		perror("Invalid number of arguments");
 		exit(98);
 	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		return (1);
+	offset = lseek(fd, 0, SEEK_SET);
 	if (offset == (off_t)-1)
 	{
 		perror("Error seeking to beginning of file");
@@ -200,13 +195,12 @@ int main(int argc, char *argv[])
 	}
 	/*print out the ELF header*/
 	printf("ELF Header:\n");
-	elf_identity();
-	ver();
-	osabi();
-	abi();
-	type_and_entry();
+	elf_identity(header);
+	ver(header);
+	osabi(header);
+	abi(header);
+	type_and_entry(header);
 
-	free(bufffer);
 	close(fd);
-	return 0;
+	return (0);
 }
